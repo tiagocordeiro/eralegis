cb53f13776f04f448e79f093dcb864f701360caf
Tiago Cordeiro <tiago@mulhergorila.com>
2018-04-24 03:04:51 -0300

    Initial Commit

---
+++ eralegis/thelemicdate.py
@@ -0,0 +1,56 @@
+import time
+from datetime import date
+
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+
+# Era Vulgar Year
+ev_year = int(date.today().strftime('%Y'))
+
+# Whole Years since March Equinox 1904
+ev_years_total = ev_year - 1904
+
+# New Aeon "generation" of 22 years
+ciclo_i = ev_years_total // 22
+
+# // Years in the current cycle
+ciclo_ii = ev_year - 1904 - (ciclo_i * 22)
+
+numerals = ['0', 'i', 'ii', 'iii', 'iv',
+            'v', 'vi', 'vii', 'viii', 'ix',
+            'x', 'xi', 'xii', 'xiii', 'xiv',
+            'xv', 'xvi', 'xvii', 'xviii', 'xix',
+            'xx', 'xxi', 'xxii']
+
+signs = {'Aries': '♈', 'Taurus': '♉', 'Gemini': '♊', 'Cancer': '♋',
+         'Leo': '♌', 'Virgo': '♍', 'Libra': '♎', 'Scorpio': '♏',
+         'Sagittarius': '♐', 'Capricorn': '♑', 'Aquarius': '♒',
+         'Pisces': '♓'}
+
+dies = ['Lunae', 'Martis', 'Mercurii', 'Jovis', 'Veneris', 'Saturnii', 'Solis']
+
+# New Aeon year
+na_year = numerals[ciclo_ii].upper() + ':' + numerals[ciclo_i]
+
+ev_weekday = date.today().weekday()
+ev_today = str(date.today().strftime('%Y/%m/%d'))
+ev_time = str(time.strftime('%H:%M'))
+
+na_date = Datetime(ev_today, ev_time, '+03:00')
+pos = GeoPos('23s39', '46w32')
+chart = Chart(na_date, pos)
+
+sun = chart.getObject(const.SUN)
+solis = str(sun).split(' ')
+solis_sign = solis[1]
+solis_arc = solis[2].split(':')[0].replace('+', '')
+
+moon = chart.get(const.MOON)
+luna = str(moon).split(' ')
+luna_sign = luna[1]
+luna_arc = luna[2].split(':')[0].replace('+', '')
+
+retorno = f'☉ in {solis_arc}º {signs[solis_sign]} ☽ in {luna_arc}º {signs[luna_sign]} Dies {dies[ev_weekday]} Anno {na_year} æræ novæ'
+print(f'☉ in {solis_arc}º {signs[solis_sign]} ☽ in {luna_arc}º {signs[luna_sign]} Dies {dies[ev_weekday]} Anno {na_year} æræ novæ')
