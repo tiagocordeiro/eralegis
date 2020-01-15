3ff367c50f415c697310a7269d0fc300e5b9b6ff
Tiago Cordeiro <tiago@mulhergorila.com>
2020-01-15 13:36:13 -0300

    New feature.
    - Função in_day, retorna data telemica de um dia específico.
    - Melhora cobertura de testes.

--- eralegis/thelemicdate.py
+++ eralegis/thelemicdate.py
@@ -6,6 +6,20 @@ from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 
+numerals = ['0', 'i', 'ii', 'iii', 'iv',
+            'v', 'vi', 'vii', 'viii', 'ix',
+            'x', 'xi', 'xii', 'xiii', 'xiv',
+            'xv', 'xvi', 'xvii', 'xviii', 'xix',
+            'xx', 'xxi', 'xxii']
+
+signs = {'Aries': '♈', 'Taurus': '♉', 'Gemini': '♊', 'Cancer': '♋',
+         'Leo': '♌', 'Virgo': '♍', 'Libra': '♎', 'Scorpio': '♏',
+         'Sagittarius': '♐', 'Capricorn': '♑', 'Aquarius': '♒',
+         'Pisces': '♓'}
+
+dies = ['Lunae', 'Martis', 'Mercurii', 'Jovis',
+        'Veneris', 'Saturnii', 'Solis']
+
 
 def now():
     """ Função não recebe parametros
@@ -22,20 +36,6 @@ def now():
 
     # // Years in the current cycle
     ciclo_ii = ev_year - 1904 - (ciclo_i * 22)
-
-    numerals = ['0', 'i', 'ii', 'iii', 'iv',
-                'v', 'vi', 'vii', 'viii', 'ix',
-                'x', 'xi', 'xii', 'xiii', 'xiv',
-                'xv', 'xvi', 'xvii', 'xviii', 'xix',
-                'xx', 'xxi', 'xxii']
-
-    signs = {'Aries': '♈', 'Taurus': '♉', 'Gemini': '♊', 'Cancer': '♋',
-             'Leo': '♌', 'Virgo': '♍', 'Libra': '♎', 'Scorpio': '♏',
-             'Sagittarius': '♐', 'Capricorn': '♑', 'Aquarius': '♒',
-             'Pisces': '♓'}
-
-    dies = ['Lunae', 'Martis', 'Mercurii', 'Jovis',
-            'Veneris', 'Saturnii', 'Solis']
 
     # New Aeon year
     na_year = numerals[ciclo_ii].upper() + ':' + numerals[ciclo_i]
@@ -64,6 +64,32 @@ def now():
             f'Anno {na_year} æræ novæ')
 
 
+def in_day(year, month, day, hour, minute):
+    ev_in_day_weekday = date(year, month, day).weekday()
+    ev_in_day_date = str(date(year, month, day).strftime('%Y/%m/%d'))
+    ev_in_day_time = f'{hour}:{minute}'
+    ev_in_day_na_year = int(year) - 1904
+
+    na_date = Datetime(ev_in_day_date, ev_in_day_time, '-03:00')
+    pos = GeoPos('23s39', '46w32')
+    chart = Chart(na_date, pos)
+
+    sun = chart.getObject(const.SUN)
+    solis = str(sun).split(' ')
+    solis_sign = solis[1]
+    solis_arc = solis[2].split(':')[0].replace('+', '')
+
+    moon = chart.get(const.MOON)
+    luna = str(moon).split(' ')
+    luna_sign = luna[1]
+    luna_arc = luna[2].split(':')[0].replace('+', '')
+
+    return (f'☉ in {solis_arc}º {signs[solis_sign]} '
+            f'☽ in {luna_arc}º {signs[luna_sign]} '
+            f'Dies {dies[ev_in_day_weekday]} '
+            f'Anno {ev_in_day_na_year} æræ novæ')
+
+
 if __name__ == '__main__':
-    hoje = now()
-    print(hoje)
+    agora = now()
+    print(agora)
